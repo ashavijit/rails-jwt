a6f9ac8b62d06c331a6202bf173bb5d958f8d056
ashavijit <as2440@it.jgec.ac.in>
2023-02-20 20:14:12 +0530

    auth-fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,19 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
   get '/*a', to: 'application#not_found
 end
+
+# Path: app/controllers/application_controller.rb
+class ApplicationController < ActionController::API
+
+  before_action :authorize_request
+  attr_reader :current_user
+
+  private
+
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+
+  def not_found
+    render json: { error: 'not_found' }
+  end
